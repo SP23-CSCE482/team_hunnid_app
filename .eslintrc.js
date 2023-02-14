ec5c0cc8f5140a16b4b9fc23a905b0939cb87b1f
dan-choi935 <lee621daniel@gmail.com>
2023-02-13 22:29:34 -0600

    Changes to eslint
    Edited Eslint to ignore unused vars as errors that prevent compilation. Instead warnings are thrown. Also ignores if React header not included in js files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-unused-vars": "warn",
+        "react/react-in-jsx-scope": "off"
+    }
+}
