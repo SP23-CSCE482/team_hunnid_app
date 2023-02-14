a4412c947bd0a7af5dcb16ca88c2114c0fe38b70
JakaTan <jakaTanugraha@tamu.edu>
2023-02-14 17:21:18 -0600

    made changes to upload.js classname class

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React from 'react'
+import { Button } from '@material-ui/core'
 
 function FileUpload() {
-    const handleUpload =() => {
-    alert("File uploaded")
-    };
-    return (
-      <div className="FileUploadComp">
-          <form onSubmit={handleUpload}>
-            <h1 class="text-white" >Upload a file</h1>
-            <input type="file" />
-            <Button variant="contained" type="submit">Upload</Button>
-          </form>
-      </div> 
-    );
+  const handleUpload = () => {
+    alert('File uploaded')
   }
-export default FileUpload;
+  return (
+    <div className="FileUploadComp">
+      <form onSubmit={handleUpload}>
+        <h1 className="text-white">Upload a file</h1>
+        <input type="file" />
+        <Button variant="contained" type="submit">
+          Upload
+        </Button>
+      </form>
+    </div>
+  )
+}
+export default FileUpload
