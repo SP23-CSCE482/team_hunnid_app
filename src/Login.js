0d1818a760fca4f76de04596ec49b9df0abc9a5a
ChristopherPasala <christopher.pasala@gmail.com>
2023-02-13 21:29:11 -0600

    upload feature

--- src/Login.js
+++ src/Login.js
@@ -14,6 +14,7 @@ const Login = () =>{
                 <Button variant="contained" onClick={navigateLogin}>
                     Sign-In
                 </Button>
+                <br></br>
                 <Button variant="contained">
                     Sign-Up
                 </Button>
