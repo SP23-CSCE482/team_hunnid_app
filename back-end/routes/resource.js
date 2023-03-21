46018b7a303fd5193f5bfca4a6fdc5c27cc17c48
vpranav <vpranav@tamu.edu>
2023-03-21 15:55:01 -0500

    added connection to youtube api to get video links

--- back-end/routes/resource.js
+++ back-end/routes/resource.js
@@ -9,4 +9,6 @@ router.get('/resource/findByTag/:tag', resourceController.findResourcesByTag);
 
 router.get('/resource/findByTagThroughWebscraping/:tag', resourceController.findResourcesByTagThroughWebscraping);
 
+router.get('/resource/findVideoResources/:tag', resourceController.findVideoResources);
+
 module.exports = router; // export to use in server.js
